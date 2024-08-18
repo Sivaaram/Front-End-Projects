767a67ec6882818c690fe8290e9f8c035cd46071
Sivaaram <126768351+Sivaaram@users.noreply.github.com>
2024-08-18 10:58:59 +0530

    script.js

--- Weather/script.js
+++ Weather/script.js
@@ -4,10 +4,14 @@ const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchbox = document.querySelector('.search input');
 const searchbtn = document.querySelector('.search button');
 const weathericon =document.querySelector('.weather-icon');
+const bg = document.getElementById('bg');
+
 
 async function updateWeather(city){
     const weather= await fetch(apiURL + city + `&appid=${apiKey}`); 
     var data= await weather.json();
+    console.log(data.weather[0].main)
+    
     if(searchbox.value===""){
         alert("Enter Your City Name");
     }
@@ -23,18 +27,31 @@ async function updateWeather(city){
     
         if(data.weather[0].main=='Clouds'){
             weathericon.src ="./images/clouds.png";
+            bg.style.backgroundImage="url(./images/bgclouds.jpg)";
         }
         else if(data.weather[0].main=='Clear'){
             weathericon.src ="./images/clear.png";
+            bg.style.backgroundImage="url(./images/bgclear.jpg)";
         }
         else if(data.weather[0].main=='Rain'){
             weathericon.src ="./images/rain.png";
+            bg.style.backgroundImage="url(./images/bgrain.jpg)";
+        }
+        else if(data.weather[0].main=='Snow'){
+            weathericon.src ="./images/snow.png";
+            bg.style.backgroundImage="url(./images/bgsnow.jpg)";
         }
         else if(data.weather[0].main=='Drizzle'){
             weathericon.src ="./images/drizzle.png";
+            bg.style.backgroundImage="url(./images/bgdrizzle.jpg)";
         }
         else if(data.weather[0].main=='Mist'){
-            weathericon.src ="./images/Mist.png";
+            weathericon.src ="./images/mist.png";
+            bg.style.backgroundImage="url(./images/bgmist.jpg)";
+        }
+        else if(data.weather[0].main=='Haze'){
+            weathericon.src ="./images/mist.png";
+            bg.style.backgroundImage="url(./images/bghaze.jpg)";
         }
         document.querySelector('.weather').style.display = "block";
         document.querySelector('.error').style.display = "none";
